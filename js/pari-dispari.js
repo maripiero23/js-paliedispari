d57e9644adb2974846de65f8761f60d4d6e01f58
maripiero23 <mari.pieropan@gmail.com>
2022-11-09 08:50:02 +0100

    continuando pari e dispari

--- js/pari-dispari.js
+++ js/pari-dispari.js
@@ -14,11 +14,37 @@
 
 const giocaBtn = document.getElementById("btn-play");
 
+//Creazione numero random per Pc
 let randomPc = Math.round(Math.random() * 100);
 console.log("numero del pc:", randomPc );
 
+//Creazione numero random per Utente
+let randomPlayer = Math.round(Math.random() * 100);
+console.log("numero del pc:", randomPlayer);
 
+//Sommo i 2 numeri
+const somma = randomPc + randomPlayer;
+console.log("la somma dei 2 numeri è:", somma);
 
+//Controllo se il numero è pari o dispari
+function getPariDispari(num){
+
+    /**
+     * @param {number} num
+     * @return {string}
+     */
+
+    if(isNaN(somma)){
+        return;
+
+    }else if(somma % 2 === 0){
+        return "la sommma è un numero pari";
+
+    }else{
+        return "la sommma è un numero dispari";
+
+    }
+}
 
 
 
